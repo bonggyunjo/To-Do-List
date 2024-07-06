30dd23ac340f27e0d9542dcd51016927996bb4ed
jbg <kjbg4565388@gmail.com>
2024-07-07 02:22:55 +0900

    feat: 회원가입 Controller 클래스 로직 구현

--- src/main/java/com/example/TodoList/controller/UserController.java
+++ src/main/java/com/example/TodoList/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.example.TodoList.controller;
 
+import com.example.TodoList.dto.SignUpDto;
+import com.example.TodoList.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@RequestBody SignUpDto signUpDto) {
+        try {
+            userService.registerUser(signUpDto);
+            return new ResponseEntity<>("회원가입에 성공하였습니다.", HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
