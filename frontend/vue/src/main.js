f4d375b1913dccc008c4e0a49560d4c101f15162
jbg <kjbg4565388@gmail.com>
2024-08-11 21:46:50 +0900

    feat: bootstrap install 및 UI 개선

--- frontend/vue/src/main.js
+++ frontend/vue/src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
 import store from './store/index.js'; // store를 올바르게 import
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
