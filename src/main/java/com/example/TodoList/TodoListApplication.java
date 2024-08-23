71022451799c7d015c39a33aa4ec19c59de35434
jbg <kjbg4565388@gmail.com>
2024-08-23 20:22:56 +0900

    Initialize backend/develop branch with project structure

--- src/main/java/com/example/TodoList/TodoListApplication.java
+++ src/main/java/com/example/TodoList/TodoListApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoListApplication {
-
+	//run
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
