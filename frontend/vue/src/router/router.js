9180a071b2ac3d275f742ae420a91de9404f7128
jbg <kjbg4565388@gmail.com>
2024-09-20 21:23:26 +0900

    fix: 응답 본체에 닉네임 값도 담기도록 수정

--- frontend/vue/src/router/router.js
+++ frontend/vue/src/router/router.js
@@ -9,16 +9,16 @@ import PasswordChange from "@/view/MyPage/PasswordChange.vue";
 import NicknameChange from "@/view/MyPage/NicknameChange.vue";
 import store from "@/store";
 Vue.use(VueRouter)
-/*  경로 설정*/
+
 const routes = [
-    { path: '/signup', component: SignUp, meta: { hideHeader: true,hideFooter: true }},
-    { path: '/login', component: UserLogin, meta: { hideHeader: true,hideFooter: true }},
+    { path: '/signup', component: SignUp, meta: { hideHeader: true,hideFooter: true}},
+    { path: '/login', component: UserLogin, meta: { hideHeader: true,hideFooter: true}},
     { path: '/' , component: Home},
-    { path: '/mypage', component: MyPage, meta: { hideHeader: true,hideFooter: true, requiresAuth: true }},
-    { path: '/mypage/user/delete', component: UserDelete, meta: { hideHeader: true,hideFooter: true }},
-    { path: '/mypage/user/passwordchange', component: PasswordChange, meta: { hideHeader: true,hideFooter: true }},
-    { path: '/mypage/user/nicknamechange', component: NicknameChange, meta: { hideHeader: true,hideFooter: true }},
-    { path: '/' , component: Home, meta: { hideHeader: false,hideFooter: false }},
+    { path: '/mypage', component: MyPage, meta: { hideHeader: true,hideFooter: true, requiresAuth: true}},
+    { path: '/mypage/user/delete', component: UserDelete, meta: { hideHeader: true,hideFooter: true, requiresAuth: true}},
+    { path: '/mypage/user/passwordchange', component: PasswordChange, meta: { hideHeader: true,hideFooter: true, requiresAuth: true}},
+    { path: '/mypage/user/nicknamechange', component: NicknameChange, meta: { hideHeader: true,hideFooter: true, requiresAuth: true}},
+    { path: '/' , component: Home, meta: { hideHeader: false,hideFooter: false}},
     { path: '/' , component: Home, meta: { hideFooter: false,hideHeader: false}},
 ]
 
@@ -30,9 +30,9 @@ router.beforeEach((to, from, next) => {
     const isAuthenticated = store.getters.isLogin; // Vuex에서 로그인 상태 가져오기
 
     if (requiresAuth && !isAuthenticated) {
-        next('/login'); // 로그인하지 않은 경우 로그인 페이지로 리다이렉트
+        next('/login');
     } else {
-        next(); // 인증된 경우 계속 진행
+        next();
     }
 });
 export default router
