e446c83b9b9532fc10d7e0ca5ffc433592c0bdf6
jbg <kjbg4565388@gmail.com>
2024-10-24 19:55:09 +0900

    fix: 게시글 수정 날짜에 대한 컬럼 추가 및 내용 글자 제한을 255자로 변경

--- src/main/java/com/example/TodoList/entity/Board.java
+++ src/main/java/com/example/TodoList/entity/Board.java
@@ -24,7 +24,7 @@ public class Board {
     @JoinColumn(name = "userId", nullable = false)
     private User user;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String title;
 
     private String content;
@@ -33,8 +33,18 @@ public class Board {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    // 게시글 등록 날짜 생성
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+    }
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // 게시글 수정 날짜 변경 시
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
